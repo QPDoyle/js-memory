4cd906bcf3edce2ec1c4968f21d76117cdc504b9
QPDoyle <qpdoyle13@gmail.com>
2024-10-07 11:49:53 -0400

    Add index.html and index.js files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+My Object: Soccer Player
+Name: Messi
+Age: 37
+Injured? False
+*/
+
+const player = {name:"Messi", age:37, injury:false};
+
+document.getElementById("playerName").innerHTML = player.name;
+document.getElementById("playerAge").innerHTML= player.age;
+document.getElementById("playerInjury").innerHTML = player.injury;
